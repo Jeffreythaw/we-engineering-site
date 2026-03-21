93c46309ec2a15521196f1d8804420f8baf2f5fb
Jeffrey Thaw <jeffreythaw24@gmail.com>
2026-03-22 01:57:56 +0800

    Fix production build and SEO metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import AppRoutes from "./AppRoutes";
